58222995f630899aa33b980c4acebe0cce4a7a7e
Krieglied <41011777+Krieglied@users.noreply.github.com>
2020-12-01 10:46:12 -0600

    Update DayOnePartOne.ps1

--- AdventCode/2018/DayOne/DayOnePartOne.ps1
+++ AdventCode/2018/DayOne/DayOnePartOne.ps1
@@ -5,34 +5,3 @@ foreach($line in Get-content .\freq.txt)
     $total += [int]$line
 }
 Write-Host $total
-
-DayOnePartTwo.ps1
-
-$total = 0
-$found = @{}
-$match = $false
-$continue = $true
-
-while($match -eq $false)
-{
-    foreach($line in Get-content .\freq.txt)
-    {
-        $total += [int]$line
-        if(!$found.ContainsKey($total))
-        {
-            $found.Add($total, 1)
-        }
-        else
-        {
-            Write-Host "Frequency Found!"
-            Write-Host $total
-            $match = $true
-            break
-        }
-    }
-    if($continue)
-    {
-        Write-Host $total
-        $continue = $false
-    }
-}

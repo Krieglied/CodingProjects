d10e6d8e6febeba8974e9fd8dbd004016635e147
Krieglied <41011777+Krieglied@users.noreply.github.com>
2020-12-01 10:45:45 -0600

    Create DayOnePartOne.ps1

---
+++ AdventCode/2018/DayOne/DayOnePartOne.ps1
@@ -0,0 +1,38 @@
+$total = 0
+
+foreach($line in Get-content .\freq.txt)
+{
+    $total += [int]$line
+}
+Write-Host $total
+
+DayOnePartTwo.ps1
+
+$total = 0
+$found = @{}
+$match = $false
+$continue = $true
+
+while($match -eq $false)
+{
+    foreach($line in Get-content .\freq.txt)
+    {
+        $total += [int]$line
+        if(!$found.ContainsKey($total))
+        {
+            $found.Add($total, 1)
+        }
+        else
+        {
+            Write-Host "Frequency Found!"
+            Write-Host $total
+            $match = $true
+            break
+        }
+    }
+    if($continue)
+    {
+        Write-Host $total
+        $continue = $false
+    }
+}

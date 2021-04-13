f77cd376048a651be8feebe3560c9431dfd1f6e4
Krieglied <41011777+Krieglied@users.noreply.github.com>
2021-04-13 17:56:44 -0500

    Update Generate_Path_Hash_CSV.ps1

--- Misc_Projects/File_Recovery/Generate_Path_Hash_CSV.ps1
+++ Misc_Projects/File_Recovery/Generate_Path_Hash_CSV.ps1
@@ -1,30 +1,30 @@
 # The CSV used to hold file path and MD5 Hash for the set of recovered files
-if ((Test-Path E:\Recovered_Files_List.csv) -eq $False)
+if ((Test-Path 'Recovered_Files.csv') -eq $False)
 {
-    New-Item E:\Recovered_Files_List.csv -ItemType "File" | Out-Null
-    Add-Content -Path E:\Recovered_Files_List.csv -Value '"FullPath","MD5Hash"'
+    New-Item 'Recovered_Files.csv' -ItemType "File" | Out-Null
+    Add-Content -Path 'Recovered_Files.csv' -Value '"FullPath","MD5Hash"'
 }
 
-$files = Get-ChildItem 'E:\Recovered_Files\' -Recurse -File | Select FullName
+$files = Get-ChildItem 'Recovered_Files\' -Recurse -File | Select FullName
 Write-Host "WRITING TO FILE"
 foreach($file in $files)
 {
     $name = $file.FullName
     $hash = (Get-FileHash -Algorithm MD5 -Path $name).Hash
-    Add-Content -Path E:\Recovered_Files_List.csv -Value "$name,$hash"
+    Add-Content -Path 'Recovered_Files.csv' -Value "$name,$hash"
 }
 
 # The CSV used to hold file path and MD5 Hash for the set of known files, in the file storage area
-if ((Test-Path E:\Known_Files_List.csv) -eq $False)
+if ((Test-Path 'Known_Files.csv') -eq $False)
 {
-    New-Item E:\Known_Files_List.csv -ItemType "File" | Out-Null
-    Add-Content -Path E:\Known_Files_List.csv -Value '"FullPath","MD5Hash"'
+    New-Item 'Known_Files.csv' -ItemType "File" | Out-Null
+    Add-Content -Path 'Known_Files.csv' -Value '"FullPath","MD5Hash"'
 }
-$files = Get-ChildItem '\\192.168.86.80\Network Share\Content\' -Recurse -File | Select FullName
+$files = Get-ChildItem 'Known_Files\' -Recurse -File | Select FullName
 Write-Host "WRITING TO FILE"
 foreach($file in $files)
 {
     $name = $file.FullName
     $hash = (Get-FileHash -Algorithm MD5 -Path $name).Hash
-    Add-Content -Path E:\Known_Files_List.csv -Value "$name,$hash"
+    Add-Content -Path 'Known_Files.csv' -Value "$name,$hash"
 }

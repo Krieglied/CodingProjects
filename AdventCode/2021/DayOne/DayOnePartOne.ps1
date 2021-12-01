ac9af56513a95954359b1a2bdf3399a74240e7e4
Krieglied <41011777+Krieglied@users.noreply.github.com>
2021-12-01 09:26:00 -0600

    Add files via upload

---
+++ AdventCode/2021/DayOne/DayOnePartOne.ps1
@@ -0,0 +1,12 @@
+﻿$layers = Get-Content .\day1_input.txt
+$counter = 1
+$previous = $layers[0]
+foreach ($layer in $layers)
+{
+    if($layer -gt $previous)
+    {
+        $counter++
+    }
+    $previous = $layer
+}
+$counter
---
+++ AdventCode/2021/DayOne/DayOnePartTwo.ps1
@@ -0,0 +1,18 @@
+﻿$layers = Get-Content .\day1_input.txt
+$counter = 0
+$index = 0
+foreach($layer in $layers)
+{
+    $top = [int]$layers[$index] + [int]$layers[$index+1] + [int]$layers[$index+2]
+    $bottom = [int]$layers[$index+1] + [int]$layers[$index+2] + [int]$layers[$index+3]
+    if($bottom -gt $top)
+    {
+        $counter++        
+    }
+    $index++
+    if(($layers.Length - $index) -lt 2)
+    {
+        break
+    }
+}
+$counter

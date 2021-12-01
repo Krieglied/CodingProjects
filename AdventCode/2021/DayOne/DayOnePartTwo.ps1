6d20d633cd77365711e14c79ec59196d8eac5197
Krieglied <41011777+Krieglied@users.noreply.github.com>
2021-12-01 13:19:21 -0600

    Update DayOnePartTwo.ps1

--- AdventCode/2021/DayOne/DayOnePartTwo.ps1
+++ AdventCode/2021/DayOne/DayOnePartTwo.ps1
@@ -1,7 +1,7 @@
 $layers = Get-Content .\day1_input.txt
 $counter = 0
 $index = 0
-foreach($layer in $layers)
+foreach($layer in $layers[0..($layers.Length-3)])
 {
     $top = [int]$layers[$index]
     $bottom = [int]$layers[$index+3]

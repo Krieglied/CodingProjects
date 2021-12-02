345bb13359adf6fe084c077c9cd09492a22a7bb9
Krieglied <41011777+Krieglied@users.noreply.github.com>
2021-12-02 08:04:25 -0600

    Add files via upload

---
+++ AdventCode/2021/DayTwo/DayTwoPartOne.ps1
@@ -0,0 +1,20 @@
+﻿$commands = Get-Content .\day2_input.txt
+$horz_pos = 0
+$depth = 0
+foreach($command in $commands)
+{
+    if($command.Split()[0] -eq 'forward')
+    {
+        $horz_pos += [int]$command.Split()[1]
+    }
+    if($command.Split()[0] -eq 'up')
+    {
+        $depth -= [int]$command.Split()[1]
+    }
+    if($command.Split()[0] -eq 'down')
+    {
+        $depth += [int]$command.Split()[1]
+    }
+}
+$total = $depth * $horz_pos
+$total
---
+++ AdventCode/2021/DayTwo/DayTwoPartTwo.ps1
@@ -0,0 +1,22 @@
+﻿$commands = Get-Content .\day2_input.txt
+$horz_pos = 0
+$depth = 0
+$aim = 0
+foreach($command in $commands)
+{
+    if($command.Split()[0] -eq 'forward')
+    {
+        $horz_pos += [int]$command.Split()[1]
+        $depth += $aim * [int]$command.Split()[1]
+    }
+    if($command.Split()[0] -eq 'up')
+    {
+        $aim -= [int]$command.Split()[1]
+    }
+    if($command.Split()[0] -eq 'down')
+    {
+        $aim += [int]$command.Split()[1]
+    }
+}
+$total = $depth * $horz_pos
+$total

68be334c569a7e526e99e1b5318d8c6b88dcfa2a
Krieglied <41011777+Krieglied@users.noreply.github.com>
2021-12-02 13:13:05 -0600

    Update DayOnePartOne.ps1

--- AdventCode/2021/DayOne/DayOnePartOne.ps1
+++ AdventCode/2021/DayOne/DayOnePartOne.ps1
@@ -1,9 +1,9 @@
 ﻿$layers = Get-Content .\day1_input.txt
-$counter = 1
+$counter = 0
 $previous = $layers[0]
 foreach ($layer in $layers)
 {
-    if($layer -gt $previous)
+    if([int]$layer -gt [int]$previous)
     {
         $counter++
     }

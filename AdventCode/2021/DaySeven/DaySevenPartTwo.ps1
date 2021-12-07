443c0e5dea0f46b4f6dc366e5bd4f0fc207aa6f2
Krieglied <41011777+Krieglied@users.noreply.github.com>
2021-12-07 10:06:27 -0600

    Update DaySevenPartTwo.ps1

--- AdventCode/2021/DaySeven/DaySevenPartTwo.ps1
+++ AdventCode/2021/DaySeven/DaySevenPartTwo.ps1
@@ -1,26 +1,51 @@
 $crab_pos = Get-Content .\day7_input.txt
 $crab_pos = $crab_pos.Split(',')
 $max_pos = $crab_pos | Measure-Object -Maximum
+
+$crab_pos = $crab_pos | Sort-Object
+if ($crab_pos.count %2 ) {
+    #odd
+    $median = $crab_pos[[math]::Floor($crab_pos.count / 2)]
+}
+else {
+    #even
+    $median = ($crab_pos[$crab_pos.Count / 2], $crab_pos[$crab_pos.count / 2 - 1] |Measure-Object -Average).average
+}
+
 $answer_sum = -1
-foreach($align in 0..$max_pos.Maximum)
+$range = 10
+$found = $False
+$previous_sum = -1
+while($found -eq $False)
 {
-    $current_sum = 0
-    foreach($crab in $crab_pos)
+    foreach($align in ($median - 30)..($median + 30))
     {
-        $distance = [Math]::Abs([int]$crab - $align)
-        # Part 2 introduces adding the sum of numbers, with n
-        # equal to the distance between the starting position
-        # and the new alignment
-        $current_sum += ($distance*($distance + 1)) / 2
+        $current_sum = 0
+        foreach($crab in $crab_pos)
+        {
+            $current_sum += [Math]::Abs([int]$crab - $align)
+        }
+        if($answer_sum -eq -1)
+        {
+            $answer_sum = $current_sum
+            continue
+        }
+        if($current_sum -lt $answer_sum)
+        {
+            $answer_sum = $current_sum
+        }
     }
-    if($answer_sum -eq -1)
+
+    if($previous_sum -eq -1)
     {
-        $answer_sum = $current_sum
+        $previous_sum = $answer_sum
+        $range += 10
         continue
     }
-    if($current_sum -lt $answer_sum)
+    if($previous_sum -eq $answer_sum)
     {
-        $answer_sum = $current_sum
+        $found = $True
     }
+    $range += 10
 }
 $answer_sum

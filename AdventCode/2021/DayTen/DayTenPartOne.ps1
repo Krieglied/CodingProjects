0f13ab808b644aebe2447ba45c0b7971033da680
Krieglied <41011777+Krieglied@users.noreply.github.com>
2021-12-10 11:20:52 -0600

    Update DayTenPartOne.ps1

--- AdventCode/2021/DayTen/DayTenPartOne.ps1
+++ AdventCode/2021/DayTen/DayTenPartOne.ps1
@@ -1,6 +1,8 @@
 $nav_commands = Get-Content .\day10_input_Graham.txt
 $stack = [System.Collections.Stack]@()
 $total_sum_errors = 0
+$values = @{')'=3;']'=57;'}'=1197;'>'=25137}
+$op_commands = @{'('=')';'['=']';'{'='}';'<'='>'}
 foreach ($chunk in $nav_commands)
 {
     foreach($command in $chunk.ToCharArray())
@@ -11,33 +13,15 @@ foreach ($chunk in $nav_commands)
             $stack.Push($command)
         }
         # If one of the end commands, test to see if last element of stack matches
-        elseif(($command -eq ")" -and $stack.Peek() -eq "(") -or 
-            ($command -eq "]" -and $stack.Peek() -eq "[") -or 
-            ($command -eq "}" -and $stack.Peek() -eq "{") -or 
-            ($command -eq ">" -and $stack.Peek() -eq "<"))
+        elseif($command -eq $op_commands[$stack.Peek().ToString()])
         {
             $null = $stack.Pop()
         }
         # Otherwise, the line is corrupted.  Start to compute the score
         else {
-            if($command -eq ")")
-            {
-                $total_sum_errors += 3
-            }
-            if($command -eq "]")
-            {
-                $total_sum_errors += 57
-            }
-            if($command -eq "}")
-            {
-                $total_sum_errors += 1197
-            }
-            if($command -eq ">")
-            {
-                $total_sum_errors += 25137
-            }
+            $total_sum_errors += $values[$command.toString()]
             break
         }
     }
 }
-$total_sum_errors
+Write-Host "Total Sum of Errors :" $total_sum_errors

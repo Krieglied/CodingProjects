3917371ef421f95da61867d470e9d9bc38ef39e4
kriegliedthosshys <robert.graham.dev@gmail.com>
2022-12-06 07:36:26 -0600

    Day Six Complete!

---
+++ AdventCode/2022/DaySix/DaySixPartOne.py
@@ -0,0 +1,13 @@
+data = []
+
+with open('./data.txt', 'r') as file_object:
+    data = file_object.read()
+
+index = 0
+
+while index < len(data):
+    marker = data[index:index+4]
+    if len(marker) == len(set(marker)):
+        print(f'Start of packet at index {index + 4}')
+        break
+    index += 1

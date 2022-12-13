5cae7795cf7f47e5c697849f61c0b3fa7f5c5382
kriegliedthosshys <robert.graham.dev@gmail.com>
2022-12-13 14:11:57 -0600

    Not sure where the logic is failing, but at least it generates a number.

---
+++ AdventCode/2022/DayThirteen/DayThirteenPartOne.py
@@ -0,0 +1,82 @@
+def build_list(line):
+    temp_list = []
+    list_line = list(line)
+    list_line.pop(0)
+    while len(list_line) > 0:
+        value = list_line.pop(0)
+        if value.isnumeric():
+            temp_list.append(value)
+        if value == '[':
+            substring = value
+            while value != ']':
+                value = list_line.pop(0)
+                substring += value
+            temp_list.append(build_list(substring))
+    return temp_list
+
+def test_element(element1, element2):
+    if type(element1) != list and type(element2) != list:
+        if element1 <= element2:
+            return True
+        return False
+    if type(element1) == list and type(element2) == list:
+        while len(element1) > 0:
+            if element1 != [] and element2 != []:
+                new_element1 = element1.pop(0)
+                new_element2 = element2.pop(0)
+                total_test = test_element(new_element1, new_element2)
+                if not total_test:
+                    return False
+                if len(element1) == 0 and total_test:
+                    return True 
+            elif element2 == []:
+                return False
+        return True
+    if type(element1) == list and type(element2) != list:
+        if element1 != []:
+            if type(element1[0]) == list:
+                return test_element(element1[0], element2)
+            value1 = element1[0]
+            if int(value1) <= int(element2):
+                return True
+        return False        
+    if type(element2) == list and type(element1) != list:
+        if element2 != []:
+            if type(element2[0]) == list:
+                return test_element(element1, element2[0])
+            value2 = element2[0]
+            if int(element1) <= int(value2):
+                return True
+        return False
+        
+    
+data = []
+
+with open('./data.txt', 'r') as file_object:
+    data = file_object.read().splitlines()
+
+line_num = 0
+index = 1
+sum_indices = 0
+
+while line_num < len(data):
+    stack1 = build_list(data[line_num].replace(',', ''))
+    line_num += 1
+    stack2 = build_list(data[line_num].replace(',', ''))
+    total_test = True
+    while len(stack1) > 0 and len(stack2) > 0:
+        element1 = stack1.pop(0)
+        element2 = stack2.pop(0)
+        total_test = test_element(element1, element2)
+        if (len(stack1) == 0) and total_test:
+            break
+        if len(stack2) == 0 or not total_test:
+            break
+    if (len(stack1) == 0) and total_test or len(stack2) == 0:
+        sum_indices += index
+    line_num += 2
+    if line_num == 435:
+        print(f'LINE {line_num}')
+    index += 1
+
+print(sum_indices)
